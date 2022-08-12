8de1da8501136dc30989dcfb4ec65c95b0f61a41
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2022-08-12 23:08:59 +0530

    done lol

--- main.go
+++ main.go
@@ -7,5 +7,37 @@ import (
 )
 
 func main() {
-	
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Starting server on port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err:= r.ParseForm(); err!=nil{
+		fmt.Fprintf(w, "Form Parse error: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %s\nAddress: %s\n", name, address)
 }
